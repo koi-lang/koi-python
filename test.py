abc04986f84234698ac36bb5d62bd40edca062b5
Dibble <DeflatedPickle@gmail.com>
2018-05-28 23:29:37 +0100

    Added a test script

---
+++ test.py
@@ -0,0 +1,11 @@
+from koicore import KoiObject
+from koicore.types import KoiInteger, KoiString, KoiBoolean
+from koicore.other import KoiVariable
+
+obj = KoiObject()
+
+int_ = KoiInteger(0)
+str_ = KoiString("")
+bool_ = KoiBoolean(False)
+
+var = KoiVariable(KoiString(""))
